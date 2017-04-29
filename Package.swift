0ae081d81fa12d71e6813c4e0db88c2f160885dc
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-04-30 00:33:59 +0800

    removed test dependency

--- Package.swift
+++ Package.swift
@@ -2,10 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "RequestSession",
-    testDependencies: [
-        .Package(url: "https://github.com/neonichu/spectre-build.git", majorVersion: 0),
-    ],
     dependencies: [
-      .Package(url: "https://github.com/neonichu/Requests", majorVersion: 0),
+      .Package(url: "https://github.com/neonichu/Requests", majorVersion: 0)
     ]
 )
