0ea57af6e64eb0e961ecf90c45f44eafa65cebe1
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-04-30 00:43:48 +0800

    updated package requirement

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "RequestSession",
     dependencies: [
-      .Package(url: "https://github.com/neonichu/Requests", majorVersion: 0)
+      .Package(url: "https://github.com/neonichu/Requests", "0.1.4")
     ]
 )
