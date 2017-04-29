18033630769c5cc6587a7901f91f66265fbcf10a
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-04-30 00:32:03 +0800

    testdependency precedence

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "RequestSession",
+    testDependencies: [
+        .Package(url: "https://github.com/neonichu/spectre-build.git", majorVersion: 0),
+    ],
     dependencies: [
       .Package(url: "https://github.com/neonichu/Requests", majorVersion: 0),
-    ],
-    testDependencies: [
-      .Package(url: "https://github.com/neonichu/spectre-build.git", majorVersion: 0),
     ]
 )
